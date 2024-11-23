dc955f222e01f8729a6f0557b2315c15d632086b
Serdarcan Buyukdereli <serdarcanbuyukdereli@Serdarcan-M2-Pro.local>
2024-11-23 23:36:45 +0300

    test

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 
 const drawerWidth = 240;
-const navItems = [['Expertise', 'expertise'], ['Career', 'history'];
+const navItems = [['Expertise', 'expertise'], ['Career', 'history']];
 
 function Navigation({parentToChild, modeChange}: any) {
 
