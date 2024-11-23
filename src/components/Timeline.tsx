37f6943eb205a5e81155af88070ae7206ef91dbd
Serdarcan Buyukdereli <serdarcanbuyukdereli@Serdarcan-M2-Pro.local>
2024-11-23 22:42:28 +0300

    test

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,38 +16,51 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="2023 - present"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">arabam.com</h3>
+            <h4 className="vertical-timeline-element-subtitle">Senior DevOps Engineer</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+            • Docker, Kubernetes, Helm, Ansible, Terraform, Keycloak integration and management.<br />
+            • GitOps integration and management.<br />
+            • PostgreSQL, Kafka, Redis integration and management.<br />
+            • Prometheus with Consul integration and management.<br />
+            • CI/CD pipelines design and management.<br />
+            • MinIO setup and management.<br />
+            • Project Management and Business Development.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="2021 - 2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Insider</h3>
+            <h4 className="vertical-timeline-element-subtitle">DevOps Engineer</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+            • AWS(EC2,ECS,S3,ACM,EB,Lambda,EKS,Route53,CloudWatch) integration and management.
+            • EKS Cluster, Helm integration and management.
+            • Migrate from Elastic Beanstalk to AWS ECS
+            • AWS Terraform, Terragrunt integration and management.            
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="2020 - 2021"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">BİTES - Defense & Aerospace Technologies</h3>
+            <h4 className="vertical-timeline-element-subtitle">DevOps Engineer</h4>
             <p>
-              Full-stack Development, API Development, User Experience
+            • Docker, Kubernetes, Helm, Ansible, Envoy integration and management.
+            • AWS(EC2,ECS,S3,ACM,CloudFront,Route53,CloudWatch) integration and management.
+            • DigitalOcean Kubernetes Cluster integration and management.
+            • Postgresql, Minio, Redis integration and management.
+            • Zabbix integration and management.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -56,10 +69,30 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <h3 className="vertical-timeline-element-title">TURK AI</h3>
+            <h4 className="vertical-timeline-element-subtitle">DevOps Engineer</h4>
             <p>
-              Automation, Data Governance, Statistical Analysis
+            • Docker, Kubernetes, Gitlab CI/CD , Istio Gateway ,Nexus integration and management.
+            • Microservice integration and management.
+            • Postgresql, Minio, Redis, Rabbitmq integration and management.
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2018 - 2020"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Netgsm Communication and Information Technologies Inc</h3>
+            <h4 className="vertical-timeline-element-subtitle">DevOps Engineer</h4>
+            <p>
+            • Proxmox,Vmware, KVM integration and management.
+            • Router,Switch integration and management.
+            • Firewall, Mail Server integration and management.
+            • Docker, Kubernetes, Gitlab CI/CD integration and management.
+            • Microservice architecture integration .
+            • Prometheus , ELK stack integration and management.
+            • Bash, Python scripting.
             </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
